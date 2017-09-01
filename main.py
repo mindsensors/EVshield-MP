3989a49ceeea451f16e643359178940d0895b006
Seth T <seth10@users.noreply.github.com>
2017-09-01 12:00:39 -0400

    1:1 EVShield example

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # main.py -- put your code here!
 from EVShield import *
 
-ev = EVShield()
-ev.bank_a.motorRunRotations(which_motors        = SH_Motor_1,
-                            direction           = SH_Direction_Reverse,
-                            speed               = SH_Speed_Medium,
-                            rotations           = 2,
-                            wait_for_completion = SH_Completion_Wait_For,
-                            next_action         = SH_Next_Action_BrakeHold)
+evshield = EVShield()
+evshield.bank_a.motorRunRotations(SH_Motor_1,
+         SH_Direction_Reverse,
+         SH_Speed_Medium,
+         2,
+         SH_Completion_Wait_For,
+         SH_Next_Action_BrakeHold);
