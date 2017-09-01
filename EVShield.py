16eb05fcab95bd5a47caab5c2696be8df9733fe8
Seth T <seth10@users.noreply.github.com>
2017-09-01 11:55:19 -0400

    Remove redundant wrapper classes

--- EVShield.py
+++ EVShield.py
@@ -1,17 +1,13 @@
-# main.py -- put your code here!
-
 import struct
 import pyb
-
 
 # Defines from SHDefines.h
 
 # For the sensor bank ports
-class SH_BankPort():
-    SH_BAS1 = 0x01 # Bank A Sensor Port 1
-    SH_BAS2 = 0x02 # Bank A Sensor Port 2
-    SH_BBS1 = 0x03 # Bank B Sensor Port 1
-    SH_BBS2 = 0x04 # Bank B Sensor Port 2
+SH_BAS1 = 0x01 # Bank A Sensor Port 1
+SH_BAS2 = 0x02 # Bank A Sensor Port 2
+SH_BBS1 = 0x03 # Bank B Sensor Port 1
+SH_BBS2 = 0x04 # Bank B Sensor Port 2
 
 BTN_LEFT = 1
 BTN_GO = 2
@@ -109,29 +105,24 @@ SH_PS_TS_UNLOCK = 0x45
 
 
 # Constants to be used by user programs.
-class SH_Motor():
-    SH_Motor_1    = 0x01
-    SH_Motor_2    = 0x02
-    SH_Motor_Both = 0x03
+SH_Motor_1    = 0x01
+SH_Motor_2    = 0x02
+SH_Motor_Both = 0x03
 
-class SH_Next_Action():
-    SH_Next_Action_Float     = 0x00 # stop and let the motor coast
-    SH_Next_Action_Brake     = 0x01 # apply brakes, and resist change to tachometer, but if tach position is forcibly changed, do not restore position
-    SH_Next_Action_BrakeHold = 0x02 # apply brakes, and restore externally forced change to tachometer
+SH_Next_Action_Float     = 0x00 # stop and let the motor coast
+SH_Next_Action_Brake     = 0x01 # apply brakes, and resist change to tachometer, but if tach position is forcibly changed, do not restore position
+SH_Next_Action_BrakeHold = 0x02 # apply brakes, and restore externally forced change to tachometer
 
-class SH_Direction():
-    SH_Direction_Reverse = 0x00
-    SH_Direction_Forward = 0x01
+SH_Direction_Reverse = 0x00
+SH_Direction_Forward = 0x01
 
 # Tachometer movement related constants
-class SH_Move():
-    SH_Move_Absolute = 0x00 # Move the tach to absolute value provided
-    SH_Move_Relative = 0x01 # Move the tach relative to previous position
+SH_Move_Absolute = 0x00 # Move the tach to absolute value provided
+SH_Move_Relative = 0x01 # Move the tach relative to previous position
 
 # Whether to wait for motor to finish it's current task or not
-class SH_Completion_Wait():
-  SH_Completion_Dont_Wait = 0x00 # Don't wait for motor to finish, program will continue with next function
-  SH_Completion_Wait_For  = 0x01 # Wait for motor to finish, program will wait until current function finishes it's operation
+SH_Completion_Dont_Wait = 0x00 # Don't wait for motor to finish, program will continue with next function
+SH_Completion_Wait_For  = 0x01 # Wait for motor to finish, program will wait until current function finishes it's operation
 
 # Speed constants: these are just convenience constants, you can use any value between 0 and 100.
 SH_Speed_Full   = 90
@@ -297,13 +288,12 @@ class EVShieldBank():
     # EVShieldBank
     
     def helper(self, readOrWriteMethod, which_motor, motor1Register, motor2Register, value = None):
-        # TODO: make enums implement iterable, change to `which_motor not in SH_Motor`
-        if which_motor != SH_Motor.SH_Motor_1 and which_motor != SH_Motor.SH_Motor_2:
+        if which_motor not in [SH_Motor_1, SH_Motor_2]:
             return # invalid motor
         if value:
-            readOrWriteMethod(motor1Register if which_motor == SH_Motor.SH_Motor_1 else motor2Register, value)
+            readOrWriteMethod(motor1Register if which_motor == SH_Motor_1 else motor2Register, value)
         else:
-            return readOrWriteMethod(motor1Register if which_motor == SH_Motor.SH_Motor_1 else motor2Register)
+            return readOrWriteMethod(motor1Register if which_motor == SH_Motor_1 else motor2Register)
     
     # Voltage value returned in milli-volts.
     def evshieldGetBatteryVoltage(self):
@@ -375,25 +365,25 @@ class EVShieldBank():
         self.EVShieldIssueCommand('S')
     
     def motorResetEncoder(self, which_motor):
-        if which_motor == SH_Motor.SH_Motor_1 or which_motor == SH_Motor.SH_Motor_Both:
+        if which_motor == SH_Motor_1 or which_motor == SH_Motor_Both:
             self.EVShieldIssueCommand('r')
-        if which_motor == SH_Motor.SH_Motor_2 or which_motor == SH_Motor.SH_Motor_Both:
+        if which_motor == SH_Motor_2 or which_motor == SH_Motor_Both:
             self.EVShieldIssueCommand('s')
     
     def motorSetSpeedTimeAndControl(self, which_motors, speed, duration, control):
-        if which_motors == SH_Motor.SH_Motor_Both:
+        if which_motors == SH_Motor_Both:
             control &= ~SH_CONTROL_GO # Clear the 'go right now' flag
-            self.motorSetSpeedTimeAndControl(SH_Motor.SH_Motor_1, speed, duration, control)
-            self.motorSetSpeedTimeAndControl(SH_Motor.SH_Motor_2, speed, duration, control)
+            self.motorSetSpeedTimeAndControl(SH_Motor_1, speed, duration, control)
+            self.motorSetSpeedTimeAndControl(SH_Motor_2, speed, duration, control)
             self.motorStartBothInSync()
         else:
             self.helper(self.writeRegisters, which_motors, SH_SPEED_M1, SH_SPEED_M2, bytes([speed%256, duration, 0, control]))
     
     def motorSetEncoderSpeedTimeAndControl(self, which_motors, encoder, speed, duration, control):
-        if which_motors == SH_Motor.SH_Motor_Both: # The motor control registers are back to back, and both can be written in one command
+        if which_motors == SH_Motor_Both: # The motor control registers are back to back, and both can be written in one command
             control &= ~SH_CONTROL_GO # Clear the 'go right now' flag
-            self.motorSetEncoderSpeedTimeAndControl(SH_Motor.SH_Motor_1, encoder, speed, duration, control)
-            self.motorSetEncoderSpeedTimeAndControl(SH_Motor.SH_Motor_2, encoder, speed, duration, control)
+            self.motorSetEncoderSpeedTimeAndControl(SH_Motor_1, encoder, speed, duration, control)
+            self.motorSetEncoderSpeedTimeAndControl(SH_Motor_2, encoder, speed, duration, control)
             self.motorStartBothInSync()
         else: # Or, just issue the command for one motor
             self.helper(self.writeRegisters, which_motors, SH_SETPT_M1, SH_SETPT_M2, struct.pack('I', encoder) + bytes([speed%256, duration, 0, control]))
@@ -401,7 +391,7 @@ class EVShieldBank():
     def motorIsTimeDone(self, which_motors):
         # because sections of this method were commented out (and there are no else blocks) it always returns 0
         return 0
-        if which_motors == SH_Motor.SH_Motor_Both:
+        if which_motors == SH_Motor_Both:
             s1 = motorGetStatusByte(SH_Motor_1)
             s2 = motorGetStatusByte(SH_Motor_2)
             if s1 & SH_STATUS_TIME == 0 and s2 & SH_STATUS_TIME == 0:
@@ -426,7 +416,7 @@ class EVShieldBank():
     def motorIsTachoDone(self, which_motors):
         # because sections of this method were commented out (and there are no else blocks) it always returns 0
         return 0
-        if which_motors == SH_Motor.SH_Motor_Both:
+        if which_motors == SH_Motor_Both:
             s1 = motorGetStatusByte(SH_Motor_1)
             s2 = motorGetStatusByte(SH_Motor_2)
             if s1 & SH_STATUS_TACHO == 0 and s2 & SH_STATUS_TACHO == 0:
@@ -450,30 +440,30 @@ class EVShieldBank():
     
     def motorRunUnlimited(self, which_motors, direction, speed):
         control = SH_CONTROL_SPEED | SH_CONTROL_GO
-        if direction == SH_Direction.SH_Direction_Reverse: speed = -speed
+        if direction == SH_Direction_Reverse: speed = -speed
         self.motorSetSpeedTimeAndControl(which_motors, speed, 0, control)
     
     def motorRunSeconds(self, which_motors, direction, speed, duration, wait_for_completion, next_action):
         control = SH_CONTROL_SPEED | SH_CONTROL_TIME | SH_CONTROL_GO
-        if next_action == SH_Next_Action.SH_Next_Action_Brake: control |= SH_CONTROL_BRK
-        elif (next_action == SH_Next_Action.SH_Next_Action_BrakeHold): control |= SH_CONTROL_BRK | SH_CONTROL_ON
+        if next_action == SH_Next_Action_Brake: control |= SH_CONTROL_BRK
+        elif (next_action == SH_Next_Action_BrakeHold): control |= SH_CONTROL_BRK | SH_CONTROL_ON
         
-        if direction == SH_Direction.SH_Direction_Reverse: speed = -speed
+        if direction == SH_Direction_Reverse: speed = -speed
         self.motorSetSpeedTimeAndControl(which_motors, speed, duration, ctrl)
-        if wait_for_completion == SH_Completion_Wait.SH_Completion_Wait_For:
+        if wait_for_completion == SH_Completion_Wait_For:
             self.motorWaitUntilTimeDone(which_motors)
     
     def motorRunTachometer(self, which_motors, direction, speed, tachometer, relative, wait_for_completion, next_action):
         control = SH_CONTROL_SPEED | SH_CONTROL_TACHO | SH_CONTROL_GO
-        if next_action == SH_Next_Action.SH_Next_Action_Brake: control |= SH_CONTROL_BRK
-        elif (next_action == SH_Next_Action.SH_Next_Action_BrakeHold): control |= SH_CONTROL_BRK | SH_CONTROL_ON
+        if next_action == SH_Next_Action_Brake: control |= SH_CONTROL_BRK
+        elif (next_action == SH_Next_Action_BrakeHold): control |= SH_CONTROL_BRK | SH_CONTROL_ON
         
-        if direction == SH_Direction.SH_Direction_Reverse: speed = -speed
+        if direction == SH_Direction_Reverse: speed = -speed
         
         # The tachometer can be absolute or relative.
         # If it is absolute, we ignore the direction parameter.
 
-        if relative == SH_Move.SH_Move_Relative:
+        if relative == SH_Move_Relative:
             control |= SH_CONTROL_RELATIVE
             # a (relative) forward command is always a positive tachometer reading
             final_tach = abs(tachometer)
@@ -481,20 +471,20 @@ class EVShieldBank():
                 tachometer = -tachometer
 
         self.motorSetEncoderSpeedTimeAndControl(which_motors, tachometer, speed, 0, control)
-        if wait_for_completion == SH_Completion_Wait.SH_Completion_Wait_For:
+        if wait_for_completion == SH_Completion_Wait_For:
             self.motorWaitUntilTachoDone(which_motors)
     
     def motorRunDegrees(self, which_motors, direction, speed, degrees, wait_for_completion, next_action):
-        self.motorRunTachometer(which_motors, direction, speed, degrees, SH_Move.SH_Move_Relative, wait_for_completion, next_action)
+        self.motorRunTachometer(which_motors, direction, speed, degrees, SH_Move_Relative, wait_for_completion, next_action)
     
     def motorRunRotations(self, which_motors, direction, speed, rotations, wait_for_completion, next_action):
-        self.motorRunTachometer(which_motors, direction, speed, 360 * rotations, SH_Move.SH_Move_Relative, wait_for_completion, next_action)
+        self.motorRunTachometer(which_motors, direction, speed, 360 * rotations, SH_Move_Relative, wait_for_completion, next_action)
     
     def motorStop(self, which_motors, next_action):
-        if (which_motor == SH_Motor.SH_Motor_1 or which_motor == SH_Motor.SH_Motor_2 or which_motor == SH_Motor.SH_Motor_Both) \
-        and (next_action == SH_Next_Action.SH_Next_Action_Float or next_action == SH_Next_Action.SH_Next_Action_Brake or next_action == SH_Next_Action.SH_Next_Action_BrakeHold):
+        if (which_motor == SH_Motor_1 or which_motor == SH_Motor_2 or which_motor == SH_Motor_Both) \
+        and (next_action == SH_Next_Action_Float or next_action == SH_Next_Action_Brake or next_action == SH_Next_Action_BrakeHold):
             # These magic numbers are documented in the advanced development guide, "Supported I2C Commands" table
-            base_code = 'A' - 1 if next_action != SH_Next_Action.SH_Next_Action_Float else 'a' - 1
+            base_code = 'A' - 1 if next_action != SH_Next_Action_Float else 'a' - 1
             self.EVShieldIssueCommand(base_code + which_motors)
     
     
--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from EVShield import *
 
 ev = EVShield()
-ev.bank_a.motorRunRotations(which_motors = SH_Motor.SH_Motor_1, 
-                            direction = SH_Direction.SH_Direction_Reverse, 
-                            speed = SH_Speed_Medium, 
-                            rotations = 2, 
-                            wait_for_completion = SH_Completion_Wait.SH_Completion_Wait_For, 
-                            next_action = SH_Next_Action.SH_Next_Action_BrakeHold)
+ev.bank_a.motorRunRotations(which_motors        = SH_Motor_1,
+                            direction           = SH_Direction_Reverse,
+                            speed               = SH_Speed_Medium,
+                            rotations           = 2,
+                            wait_for_completion = SH_Completion_Wait_For,
+                            next_action         = SH_Next_Action_BrakeHold)
