092b102842f296fec13329d995f971c9619f37cc
Seth T <seth10@users.noreply.github.com>
2017-09-05 10:35:52 -0400

    Add pingEV functionality
    The EVShield is supposed to stop any motors if it doesn't recieve any I2C communication for a period, in case the controller dies and can't tell the EVShield to stop.

--- boot.py
+++ boot.py
@@ -3,4 +3,9 @@
 
 import pyb
 
+from EVShield import SH_Bank_A
+pingEV = pyb.Timer(1)
+pingEV.init(freq=3)
+pingEV.callback(lambda _: pyb.I2C(1).is_ready(SH_Bank_A>>1))
+
 pyb.main('main.py') # main script to run after this one
