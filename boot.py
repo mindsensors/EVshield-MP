91e57e46d2027f963e7e49266f57e9f4b495cbae
Seth T <seth10@users.noreply.github.com>
2017-09-21 15:49:44 -0400

    Implement NumericPad
    Untested

--- boot.py
+++ boot.py
@@ -13,11 +13,7 @@ pingEV.callback(lambda _: pyb.I2C(1).is_ready(SH_Bank_A>>1))
 #from EVShieldDefines import SH_BAS1
 #a = AbsoluteIMU(SH_BAS1)
 
-from EVShield import EVShield
-evshield = EVShield()
-from EVShieldSensors import NXTTouch
-from EVShieldDefines import SH_BAS1
-t = NXTTouch(evshield, SH_BAS1)
-t.isPressed()
+from EVShieldSensors import NumericPad
+n = NumericPad()
 
 #pyb.main('main.py') # main script to run after this one
