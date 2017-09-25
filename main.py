6f9b27c5a22e09c2cab0b9a60bb40574b4850c85
Seth T <seth10@users.noreply.github.com>
2017-09-25 13:16:24 -0400

    Swap out the NXTTouch sensor for an EV3Touch sensor
    Again, doesn't work on BAS1. This tests a UART sensor instead of an analog one.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # main.py -- put your code here!
 from EVShield import EVShield
-from EVShieldSensors import NXTTouch
+from EVShieldSensors import EV3Touch
 from EVShieldDefines import *
 
 evshield = EVShield()
-nxttouch = NXTTouch(evshield, SH_BAS2)
+ev3touch = EV3Touch(evshield, SH_BAS1)
 
 while not evshield.getButtonState(BTN_GO):
-    if nxttouch.isPressed():
+    if ev3touch.isPressed():
         evshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, SH_Speed_Full)
     else:
         evshield.bank_a.motorStop(SH_Motor_1, SH_Next_Action_Float)
