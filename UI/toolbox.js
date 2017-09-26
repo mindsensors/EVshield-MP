3e184184d8fe43133404deb7964d5c65bdcd6894
Seth T <seth10@users.noreply.github.com>
2017-09-26 14:46:45 -0400

    Add system_sleep

--- UI/toolbox.js
+++ UI/toolbox.js
@@ -13,7 +13,26 @@ Blockly.defineBlocksWithJsonArray([
 
 
 /* SYSTEM */
-
+{
+  "type": "system_sleep",
+  "message0": "wait for %1 seconds",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "TIME",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 5
+}
 
 ]); // end defineBlocksWithJsonArray
 
+
+Blockly.Python['system_sleep'] = function(block) {
+  var time = Blockly.Python.valueToCode(block, 'TIME', Blockly.Python.ORDER_ATOMIC);
+  Blockly.Python.definitions_.import_time = 'import time';
+  return `time.sleep(${time})\n`;
+};
