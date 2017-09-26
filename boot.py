5f5c37366a491b22575c21a7ce7be53db939dc5e
Seth T <seth10@users.noreply.github.com>
2017-09-26 13:32:14 -0400

    Move code to a code folder

--- boot.py
+++ boot.py
@@ -2,5 +2,5 @@
 # can run arbitrary Python, but best to keep it minimal
 
 import pyb
-#pyb.main('main.py') # main script to run after this one
-pyb.main('examples/key_press_count.py')
+#pyb.main('code/main.py') # main script to run after this one
+pyb.main('code/examples/key_press_count.py')
