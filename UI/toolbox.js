87e5de9b08cc2a35b717c380a7f7f2b02c99d795
Seth T <seth10@users.noreply.github.com>
2017-09-27 11:23:10 -0400

    Add button blocks

--- UI/toolbox.js
+++ UI/toolbox.js
@@ -45,7 +45,102 @@ Blockly.defineBlocksWithJsonArray([
 },
 
 /* BUTTONS */
-
+{
+  "type": "buttons_get",
+  "message0": "button %1 is pressed",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "WHICH",
+      "options": [
+        [
+          "GO",
+          "BTN_GO"
+        ],
+        [
+          "left",
+          "BTN_LEFT"
+        ],
+        [
+          "right",
+          "BTN_RIGHT"
+        ]
+      ]
+    }
+  ],
+  "output": "Boolean",
+  "colour": 200
+},
+{
+  "type": "buttons_wait",
+  "message0": "wait for button %1 to be pressed %2 with LED %3 pattern",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "WHICH",
+      "options": [
+        [
+          "GO",
+          "BTN_GO"
+        ],
+        [
+          "left",
+          "BTN_LEFT"
+        ],
+        [
+          "right",
+          "BTN_RIGHT"
+        ]
+      ]
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "field_dropdown",
+      "name": "PATTERN",
+      "options": [
+        [
+          "breathing (fade)",
+          "1"
+        ],
+        [
+          "heartbeat",
+          "2"
+        ]
+      ]
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 200
+},
+{
+  "type": "buttons_count",
+  "message0": "count times button %1 has been pressed",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "WHICH",
+      "options": [
+        [
+          "GO",
+          "BTN_GO"
+        ],
+        [
+          "left",
+          "BTN_LEFT"
+        ],
+        [
+          "right",
+          "BTN_RIGHT"
+        ]
+      ]
+    }
+  ],
+  "output": "Number",
+  "colour": 200
+},
 
 /* SYSTEM */
 {
@@ -76,12 +171,29 @@ Blockly.Python['led_set'] = function(block) {
   var blue  = parseInt(color[3], 16);
   var method;
   switch(which) {
-      case 'A': method = 'evshield.bank_a.ledSetRGB'; break;
-      case 'B': method = 'evshield.bank_b.ledSetRGB'; break;
-      case 'A+B': method = 'evshield.ledSetRGB';      break;
-      case 'C': method = 'evshield.centerLedSetRGB';  break;
+      case 'A': method = 'bank_a.ledSetRGB'; break;
+      case 'B': method = 'bank_b.ledSetRGB'; break;
+      case 'A+B': method = 'ledSetRGB';      break;
+      case 'C': method = 'centerLedSetRGB';  break;
   }
-  return `${method}(${red}, ${green}, ${blue})\n`;
+  return `evshield.${method}(${red}, ${green}, ${blue})\n`;
+};
+
+Blockly.Python['buttons_get'] = function(block) {
+  var which = block.getFieldValue('WHICH');
+  Blockly.Python.definitions_.init_evshield = "from EVShield import EVShield\nevshield = EVShield()";
+  return [`evshield.getButtonState(${which})`, Blockly.Python.ORDER_ATOMIC];
+};
+Blockly.Python['buttons_wait'] = function(block) {
+  var which = block.getFieldValue('WHICH');
+  var pattern = block.getFieldValue('PATTERN');
+  Blockly.Python.definitions_.init_evshield = "from EVShield import EVShield\nevshield = EVShield()";
+  return `evshield.waitForButtonPress(${which}, led_pattern=${pattern})\n`;
+};
+Blockly.Python['buttons_count'] = function(block) {
+  var which = block.getFieldValue('WHICH');
+  Blockly.Python.definitions_.init_evshield = "from EVShield import EVShield\nevshield = EVShield()";
+  return [`evshield.getKeyPressCount(${which})`, Blockly.Python.ORDER_ATOMIC];
 };
 
 Blockly.Python['system_sleep'] = function(block) {
