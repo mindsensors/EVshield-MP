1febd63cd927c1bca1b65dc6a6d3bdd682b83fa9
Seth T <seth10@users.noreply.github.com>
2017-09-29 10:41:41 -0400

    Add correct ping functionality
    Ping EVShield so I2C won't timeout
    Being sure not to reset the ping timer when communicating with _other_ I2C devices
    Being sure to actually return the value the wrapped function intends to

--- code/EVShieldCom.py
+++ code/EVShieldCom.py
@@ -3,41 +3,71 @@ import pyb
 import struct
 
 
+pingI2C = pyb.I2C(1, pyb.I2C.MASTER)
+def pingMethod(ignored=None):
+    try:
+        pingI2C.send(0, SH_Bank_A>>1)
+        #print("pinged")
+    except: pass
+pingTimer = pyb.Timer(4, freq=4, callback=pingMethod)
+
 class EVShieldI2C():
+    
+    def reset_ping_timeout(function):
+        def function_with_pingtimeoutreset(self, *args, **kwargs):
+            pyb.disable_irq()
+            global pingTimer
+            if self.i2c_address in [SH_Bank_A>>1, SH_Bank_B>>1]:
+                pingTimer.counter(0)
+            ret = function(self, *args, **kwargs)
+            pyb.enable_irq()
+            return ret
+        return function_with_pingtimeoutreset
+    
     def __init__(self, i2c_address):
         #pyb.I2C(1, I2C.MASTER, baudrate=20000).deinit()
         self.i2c = pyb.I2C(1)
         self.i2c.init(pyb.I2C.MASTER, baudrate=20000)
         self.i2c_address = i2c_address >> 1
-        self.timeout = 1000
+        self.timeout = 100
     
+    @reset_ping_timeout
     def readRegisters(self, register, length, timeout = None):
         return self.i2c.mem_read(length, self.i2c_address, register, timeout = timeout or self.timeout)
     
+    @reset_ping_timeout
     def readByte(self, register, timeout = None):
         return self.i2c.mem_read(1, self.i2c_address, register, timeout = timeout or self.timeout)[0]
     
+    @reset_ping_timeout
     def readInteger(self, register, timeout = None):
         return struct.unpack('h', self.i2c.mem_read(2, self.i2c_address, register, timeout = timeout or self.timeout))[0]
     
+    @reset_ping_timeout
     def readLong(self, register, timeout = None):
         return struct.unpack('l', self.i2c.mem_read(4, self.i2c_address, register, timeout = timeout or self.timeout))[0]
     
+    @reset_ping_timeout
     def readString(self, register, length, timeout = None):
         return self.i2c.mem_read(length, self.i2c_address, register, timeout = timeout or self.timeout).decode("utf-8")
     
+    @reset_ping_timeout
     def writeRegisters(self, register, data, timeout = None):
         self.i2c.mem_write(data, self.i2c_address, register, timeout = timeout or self.timeout)
     
+    @reset_ping_timeout
     def writeByte(self, register, dataByte, timeout = None):
         self.i2c.mem_write(bytes([dataByte]), self.i2c_address, register, timeout = timeout or self.timeout)
     
+    @reset_ping_timeout
     def writeInteger(self, register, dataInt, timeout = None):
         self.i2c.mem_write(struct.pack('H', dataInt), self.i2c_address, register, timeout = timeout or self.timeout)
     
+    @reset_ping_timeout
     def writeLong(self, register, dataLong, timeout = None):
         self.i2c.mem_write(struct.pack('I', dataLong), self.i2c_address, register, timeout = timeout or self.timeout)
     
+    @reset_ping_timeout
     def checkAddress(self):
         return self.i2c_address in self.i2c.scan()
     
